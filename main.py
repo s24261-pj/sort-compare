994608d38d80454565e328fb196292e7c79c6b3f
s24261-pj <s24261@pjwstk.edu.pl>
2023-05-30 20:39:14 +0200

    Improve sort algorithms

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ import heapsort
 import quicksort
 import insertionsort
 
-array = numpy.random.randint(100_000, 500_000, 1_000)
-heapsort_array = array
-quicksort_array = array
-insertionsort_array = array
+array = numpy.random.randint(100_000, 500_000, 10_000)
+heapsort_array = array.copy()
+quicksort_array = array.copy()
+insertionsort_array = array.copy()
 
 
 start_time = time.perf_counter()
-heapsort.heapsort(array)
+heapsort.heapsort(heapsort_array)
 end_time = time.perf_counter()
 print('heapsort: ', heapsort_array)
-print('Czas wykonywania heapsort: {end_time - start_time} sekund')
-
-
-start_time = time.perf_counter()
-insertionsort.insertionsort(insertionsort_array, len(insertionsort_array))
-end_time = time.perf_counter()
-print('insertionsort: ', insertionsort_array)
-print('Czas wykonywania insertionsort: {end_time - start_time} sekund')
+print(f'Czas wykonywania heapsort: {end_time - start_time} sekund')
 
 
 start_time = time.perf_counter()
 quicksort.quicksort(quicksort_array, 0, len(quicksort_array) - 1)
 end_time = time.perf_counter()
 print('quicksort: ', quicksort_array)
-print('Czas wykonywania quicksort: {end_time - start_time} sekund')
+print(f'Czas wykonywania quicksort: {end_time - start_time} sekund')
 
+
+start_time = time.perf_counter()
+insertionsort.insertionsort(insertionsort_array, len(insertionsort_array))
+end_time = time.perf_counter()
+print('insertionsort: ', insertionsort_array)
+print(f'Czas wykonywania insertionsort: {end_time - start_time} sekund')
