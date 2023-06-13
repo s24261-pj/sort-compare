900c446f038719d40df6f15e4742d787ead07674
s24261-pj <s24261@pjwstk.edu.pl>
2023-06-13 21:54:40 +0200

    Add reverse_sorted and sorted file
    - move sort algorithms to sort directory

---
+++ sorted.py
@@ -0,0 +1,29 @@
+import time
+import numpy
+
+from sorts import heapsort, insertionsort, quicksort
+
+array = numpy.random.randint(100_000, 500_000, 30_000)
+
+heapsort.heapsort(array)
+
+heapsort_array = array.copy()
+quicksort_array = array.copy()
+insertionsort_array = array.copy()
+
+start_time = time.perf_counter()
+heapsort.heapsort(heapsort_array)
+end_time = time.perf_counter()
+print(f'Czas wykonywania heapsort: {end_time - start_time} sekund')
+
+
+start_time = time.perf_counter()
+quicksort.quicksort(quicksort_array, 0, len(quicksort_array) - 1)
+end_time = time.perf_counter()
+print(f'Czas wykonywania quicksort: {end_time - start_time} sekund')
+
+
+start_time = time.perf_counter()
+insertionsort.insertionsort(insertionsort_array, len(insertionsort_array))
+end_time = time.perf_counter()
+print(f'Czas wykonywania insertionsort: {end_time - start_time} sekund')
--- unsorted.py
+++ unsorted.py
@@ -1,32 +1,26 @@
 import time
 import numpy
 
-import heapsort
-import quicksort
-import insertionsort
+from sorts import heapsort, insertionsort, quicksort
 
-array = numpy.random.randint(100_000, 500_000, 10_000)
+array = numpy.random.randint(100_000, 500_000, 30_000)
 heapsort_array = array.copy()
 quicksort_array = array.copy()
 insertionsort_array = array.copy()
 
-
 start_time = time.perf_counter()
 heapsort.heapsort(heapsort_array)
 end_time = time.perf_counter()
-print('heapsort: ', heapsort_array)
 print(f'Czas wykonywania heapsort: {end_time - start_time} sekund')
 
 
 start_time = time.perf_counter()
 quicksort.quicksort(quicksort_array, 0, len(quicksort_array) - 1)
 end_time = time.perf_counter()
-print('quicksort: ', quicksort_array)
 print(f'Czas wykonywania quicksort: {end_time - start_time} sekund')
 
 
 start_time = time.perf_counter()
 insertionsort.insertionsort(insertionsort_array, len(insertionsort_array))
 end_time = time.perf_counter()
-print('insertionsort: ', insertionsort_array)
 print(f'Czas wykonywania insertionsort: {end_time - start_time} sekund')
